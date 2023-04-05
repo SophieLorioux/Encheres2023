34ea2dca892d1e2b7536d0745446690428759e1a
mdelage2021 <melody.delage@gmail.com>
2023-04-05 17:33:36 +0200

    Cration BO Etat & Articles

---
+++ encheres/src/main/java/fr/eni/encheres/bo/Retrait.java
@@ -0,0 +1,32 @@
+package fr.eni.encheres.bo;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.Column;
+
+/**
+ * Entité représentant un retrait
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@Table(name = "retrait")
+public class Retrait {
+
+    @Id
+    @Column(name = "no_article", nullable = false)
+    private int article;
+
+    @Column(name = "rue", length = 255, nullable = false)
+    private  String rue;
+
+    @Column(name = "code_postal", length = 5, nullable = false)
+    private String codePostal;
+
+    @Column(name = "ville", length = 255, nullable = false)
+    private String ville;
+}
